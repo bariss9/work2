0f210acb8554f713124ddfce7dff7d36afd90d91
bariss9 <barissrnl@gmail.com>
2025-02-25 16:00:46 +0300

    Update basicCalculator.py

--- basicCalculator.py
+++ basicCalculator.py
@@ -1,21 +1,29 @@
-def basicCalculator(num1 , num2 , operation):
-    if(operation == '+'):
-        print(f"{num1} + {num2} = {num1 + num2}")
-    if(operation == '-'):
-         print(f"{num1} - {num2} = {num1 - num2}")
-    if(operation == '*'):
-        print(f"{num1} x {num2} = {num1 * num2}")
-    if(operation == '/'):
-        if(num2 == 0):
-            print("Second number cannot 0 for division!")
-            return False
-        print(f"{num1} / {num2} = {num1 / num2}")
+def basicCalculator(num1, num2, operation):
+    operations = {'+', '/', '*', '-'}  
+    
+    if operation in operations:
+        if operation == '+':
+            print(f"{num1} + {num2} = {num1 + num2}")
+        elif operation == '-':
+            print(f"{num1} - {num2} = {num1 - num2}")
+        elif operation == '*':
+            print(f"{num1} x {num2} = {num1 * num2}")
+        elif operation == '/':
+            if num2 == 0:
+                print("Second number cannot be 0 for division!")
+                return False
+            print(f"{num1} / {num2} = {num1 / num2}")
+    else:
+        print("Invalid Operation!")
+        return False
+        
+basicCalculator(1, 4, '+')
+basicCalculator(1, 4, '-')
+basicCalculator(1, 4, '*')
+basicCalculator(1, 4, '/')
+basicCalculator(1, 0, '/')  
+basicCalculator(1, 4, '%')  
 
-basicCalculator(1,4,'+')
-basicCalculator(1,4,'-')
-basicCalculator(1,4,'*')
-basicCalculator(1,4,'/')
-basicCalculator(1,0,'/')
 
 
 
